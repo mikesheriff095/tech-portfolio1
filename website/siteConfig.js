7b9cefc2beb9e0c2b2e3ea5ce180e4a4bba767ee
mikesheriff095 <51491958+mikesheriff095@users.noreply.github.com>
2020-11-02 20:12:09 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,15 +15,15 @@ const siteConfig = {
     title: 'Tonya A. Melvin-Bryant', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://tamelvinbryant.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    url: 'https://mikesheriff095.github.io', // Your website URL
+    baseUrl: '/tech-portfolio1/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'tamelvinbryant',
+    organizationName: 'michael sheriff',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
